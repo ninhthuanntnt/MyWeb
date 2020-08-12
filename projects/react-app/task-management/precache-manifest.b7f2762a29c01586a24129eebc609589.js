cf12f1847c9cf8311b24679354392f195f8e17c3
ninhthuanntnt <ninhthuan1999@gmail.com>
2020-08-12 21:36:47 +0700

    NTNTWeb: Update Task Management Project

---
+++ projects/react-app/task-management/precache-manifest.b7f2762a29c01586a24129eebc609589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef69ee1123b3cbf22a6409415a02d68",
+    "url": "/MyWeb/index.html"
+  },
+  {
+    "revision": "0ad49229f49e8d05b3cb",
+    "url": "/MyWeb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2056b77101f28c7bf342",
+    "url": "/MyWeb/static/js/2.35d2a5fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyWeb/static/js/2.35d2a5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad49229f49e8d05b3cb",
+    "url": "/MyWeb/static/js/main.44f118b6.chunk.js"
+  },
+  {
+    "revision": "ec8fbb3b1fa42f4d1beb",
+    "url": "/MyWeb/static/js/runtime-main.c9f48913.js"
+  }
+]);
