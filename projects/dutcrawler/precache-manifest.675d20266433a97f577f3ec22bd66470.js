53b9b0c6743592887ba95e84dbe6febadfc3a174
ninhthuanntnt <ninhthuan1999@gmail.com>
2020-09-12 15:57:47 +0700

    NTNTWeb: Update DUT Crawler Website

---
+++ projects/dutcrawler/precache-manifest.675d20266433a97f577f3ec22bd66470.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cb7a8458fd85eb5bb335fb0aeeb445",
+    "url": "/MyWeb/projects/dutcrawler/index.html"
+  },
+  {
+    "revision": "db0ea8c12767baeb8743",
+    "url": "/MyWeb/projects/dutcrawler/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "69d2343fb28c354495eb",
+    "url": "/MyWeb/projects/dutcrawler/static/css/main.357d975c.chunk.css"
+  },
+  {
+    "revision": "db0ea8c12767baeb8743",
+    "url": "/MyWeb/projects/dutcrawler/static/js/2.05a3d32e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyWeb/projects/dutcrawler/static/js/2.05a3d32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d2343fb28c354495eb",
+    "url": "/MyWeb/projects/dutcrawler/static/js/main.3dea0696.chunk.js"
+  },
+  {
+    "revision": "783890b9c6f7aff80aba",
+    "url": "/MyWeb/projects/dutcrawler/static/js/runtime-main.dfbe3c72.js"
+  },
+  {
+    "revision": "cfecb6b0985e358d0510548fbee098ae",
+    "url": "/MyWeb/projects/dutcrawler/static/media/Calendar.cfecb6b0.jpg"
+  },
+  {
+    "revision": "5e1d61e5fd12adf1d5051107bde2d996",
+    "url": "/MyWeb/projects/dutcrawler/static/media/notification.5e1d61e5.png"
+  },
+  {
+    "revision": "c4d1e3a9037f4dcb337aa63d0547eaad",
+    "url": "/MyWeb/projects/dutcrawler/static/media/score.c4d1e3a9.png"
+  }
+]);
--- projects/dutcrawler/service-worker.js
+++ projects/dutcrawler/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.779fccebae792312e530d3d062e1e073.js"
+  "/MyWeb/projects/dutcrawler/precache-manifest.675d20266433a97f577f3ec22bd66470.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyWeb/projects/dutcrawler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
